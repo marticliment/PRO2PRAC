143107058198ee8f964b3339b2ea589c7cf8f2f5
Martí Climent <marticlilop@gmail.com>
2024-05-18 00:57:12 +0200

    Move documentation to docs/

---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,39 @@
+var indexSectionsWithContent =
+{
+  0: "_abcdefghilmnopqrstuvwx",
+  1: "bcnprsv",
+  2: "bcdpsv",
+  3: "abcdefghilmnoprstuvw",
+  4: "_abcdefhilmopqrstvwx",
+  5: "n",
+  6: "lr",
+  7: "o",
+  8: "d"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "enums",
+  6: "enumvalues",
+  7: "related",
+  8: "defines"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Enumerations",
+  6: "Enumerator",
+  7: "Friends",
+  8: "Macros"
+};
+
